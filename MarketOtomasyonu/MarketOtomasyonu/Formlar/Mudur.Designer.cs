8fc844fa8e8db75df3b32223b637a0cdf6d363cc
tkursat184@gmail.com <tkursat184@gmail.com>
2022-06-04 03:31:17 +0300

    klasor olusturma

--- MarketOtomasyonu/MarketOtomasyonu/Formlar/Mudur.Designer.cs
+++ MarketOtomasyonu/MarketOtomasyonu/Formlar/Mudur.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1050, 591);
+            this.ClientSize = new System.Drawing.Size(978, 385);
             this.Name = "Mudur";
             this.Text = "Mudur";
             this.ResumeLayout(false);
