2a631b28f4f446c9d8e7f1a849bda6c154db2c39
Blackcat <Blackcat@DESKTOP-35VBHG0>
2022-06-05 16:09:11 +0300

    kayıt ol sayfası arayüz tasarımı

---
+++ MarketOtomasyonu/MarketOtomasyonu/Formlar/besir/kaydol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketOtomasyonu.Formlar.besir
+{
+	public partial class kaydol : Form
+	{
+		public kaydol()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
