4ac7ea1976173646b399224feeca9e8072e542a9
tkursat184@gmail.com <tkursat184@gmail.com>
2022-06-05 03:33:30 +0300

    Müdür ana sayfa tasarımı

---
+++ MarketOtomasyonu/MarketOtomasyonu/Formlar/kamiltrn/Mudur_Anasayfa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketOtomasyonu.Formlar.kamiltrn
+{
+    public partial class Mudur_Anasayfa : Form
+    {
+        public Mudur_Anasayfa()
+        {
+            InitializeComponent();
+        }
+    }
+}
