acdf0a8b00fca57c649ecaf0920992d063d07572
tkursat184@gmail.com <tkursat184@gmail.com>
2022-06-05 03:39:42 +0300

    Müdür ana sayfa tasarımı-2 içerik düzenlendi

--- MarketOtomasyonu/MarketOtomasyonu/Formlar/kamiltrn/Mudur_Anasayfa.Designer.cs
+++ MarketOtomasyonu/MarketOtomasyonu/Formlar/kamiltrn/Mudur_Anasayfa.Designer.cs
@@ -31,6 +31,8 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,10 +40,12 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(200, 100);
+            this.tabControl1.Size = new System.Drawing.Size(1000, 560);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -49,9 +53,9 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(192, 71);
+            this.tabPage1.Size = new System.Drawing.Size(992, 531);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Personel Takip";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
@@ -59,16 +63,36 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 71);
+            this.tabPage2.Size = new System.Drawing.Size(992, 531);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Ürün Takip";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(992, 531);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Personel Giriş/Çıkış";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(992, 531);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Market Malzeme Takip";
+            this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // Mudur_Anasayfa
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1032, 603);
             this.Controls.Add(this.tabControl1);
             this.Name = "Mudur_Anasayfa";
             this.Text = "Mudur_Anasayfa";
@@ -82,5 +106,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
     }
 }
--- MarketOtomasyonu/MarketOtomasyonu/Program.cs
+++ MarketOtomasyonu/MarketOtomasyonu/Program.cs
@@ -16,7 +16,8 @@ namespace MarketOtomasyonu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Formlar.Suleymanogrk.Personel());
+            //Application.Run(new Formlar.Suleymanogrk.Personel());
+            Application.Run(new Formlar.kamiltrn.Mudur_Anasayfa());
         }
     }
 }
