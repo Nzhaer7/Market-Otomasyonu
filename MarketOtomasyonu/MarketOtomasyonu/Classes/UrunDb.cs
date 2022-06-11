c491191a52f77694d86e24482cd659844df0cb70
Blackcat <Blackcat@DESKTOP-35VBHG0>
2022-06-11 17:46:35 +0300

    Ürün takip tablosu oluşturulması

---
+++ MarketOtomasyonu/MarketOtomasyonu/Classes/UrunDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketOtomasyonu.Classes
+{
+	public class UrunDb
+	{
+		public int id { get; set; }
+		public string kodu { get; set; }
+		public DateTime girisTarihi { get; set; }
+		public DateTime cikisTarihi { get; set; }
+		public DateTime sonKullanimTarihi { get; set; }
+		public float fiyat { get; set; }
+		public int durumu { get; set; }
+		public string adi { get; set; }
+	}
+}
--- MarketOtomasyonu/MarketOtomasyonu/Data/MOContext.cs
+++ MarketOtomasyonu/MarketOtomasyonu/Data/MOContext.cs
@@ -4,14 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using MarketOtomasyonu.Classes;
 
 namespace MarketOtomasyonu.Data
 {
 	public class MOContext : DbContext
 	{
+		public DbSet<UrunDb> Urunler { get; set; }
+
+
+
+
+
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer(@"Data Source=.; Database=MODataBase; Integrated Security=yes");
+		}
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+			modelBuilder.Entity<UrunDb>().Property(Urun => Urun.adi)
+											  .HasColumnName("Adı")
+											  .IsRequired()
+											  .HasMaxLength(30);
+			modelBuilder.Entity<UrunDb>().Property(Urun => Urun.girisTarihi)
+										  .HasColumnName("Giriş Tarihi")
+										  .IsRequired();
+			modelBuilder.Entity<UrunDb>().Property(Urun => Urun.cikisTarihi)
+										  .HasColumnName("Çıkış Tarihi")
+										  .IsRequired();
+			modelBuilder.Entity<UrunDb>().Property(Urun => Urun.kodu)
+										 .HasColumnName("Kodu")
+										  .IsRequired()
+										  .HasMaxLength(30);
+			modelBuilder.Entity<UrunDb>().Property(Urun => Urun.sonKullanimTarihi)
+										 .HasColumnName("Son Kullanma Tarihi")
+										 .IsRequired();
+			modelBuilder.Entity<UrunDb>().Property(Urun => Urun.fiyat)
+										 .HasColumnName("Fiyatı")
+										 .IsRequired()
+										 .HasMaxLength(30);
+			modelBuilder.Entity<UrunDb>().Property(Urun => Urun.durumu)
+										 .HasColumnName("Stok Durumu")
+										 .IsRequired()
+										 .HasMaxLength(30);
+
+
+
+
+
+		
 		}
 	}
 }
