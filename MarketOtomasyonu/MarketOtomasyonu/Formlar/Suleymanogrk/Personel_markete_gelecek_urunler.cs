e719fb2479eb71f65ae3f075241a5fc2666c65a2
Süleyman OĞRAK <suleymanogrk24@gmail.com>
2022-06-13 02:44:13 +0300

    Personel satışa çıkacak ürünler sayfası tasarımı

---
+++ MarketOtomasyonu/MarketOtomasyonu/Formlar/Suleymanogrk/Personel_markete_gelecek_urunler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketOtomasyonu.Formlar.Suleymanogrk
+{
+    public partial class Personel_markete_gelecek_urunler : Form
+    {
+        public Personel_markete_gelecek_urunler()
+        {
+            InitializeComponent();
+        }
+    }
+}
