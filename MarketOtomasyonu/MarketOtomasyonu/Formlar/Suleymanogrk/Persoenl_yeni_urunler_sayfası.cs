45a4ad3d4edeba6b42f9fd175a1a283ebdbe140d
Süleyman OĞRAK <suleymanogrk24@gmail.com>
2022-06-13 22:10:42 +0300

    Personel yeni ürünler sayfası tasarımı

---
+++ MarketOtomasyonu/MarketOtomasyonu/Formlar/Suleymanogrk/Persoenl_yeni_urunler_sayfası.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketOtomasyonu.Formlar.Suleymanogrk
+{
+    public partial class Persoenl_yeni_urunler_sayfası : Form
+    {
+        public Persoenl_yeni_urunler_sayfası()
+        {
+            InitializeComponent();
+        }
+
+        private void Persoenl_yeni_urunler_sayfası_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
