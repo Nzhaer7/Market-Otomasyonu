6140c4d0d1f1fe0e8ea3ca5847026f9cea687619
Blackcat <Blackcat@DESKTOP-35VBHG0>
2022-06-15 18:52:32 +0300

    giriş yap mail sayfası kodlanması

--- MarketOtomasyonu/MarketOtomasyonu/Formlar/besir/PersonelKayitOl.Designer.cs
+++ MarketOtomasyonu/MarketOtomasyonu/Formlar/besir/PersonelKayitOl.Designer.cs
@@ -95,7 +95,7 @@
 			// button1
 			// 
 			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-			this.button1.Location = new System.Drawing.Point(427, 501);
+			this.button1.Location = new System.Drawing.Point(437, 500);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(104, 44);
 			this.button1.TabIndex = 19;
